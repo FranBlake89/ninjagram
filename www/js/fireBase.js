8524e566de07590a87f764556304c716bf52a7ff
FranBlake89 <73005797+FranBlake89@users.noreply.github.com>
2021-03-08 16:40:15 -0300

    Update fireBase.js

--- www/js/fireBase.js
+++ www/js/fireBase.js
@@ -1,12 +1,11 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDPbrIV4TjU4y09El-oBH2gKExICE96jno",
-  authDomain: "ninjagram-b6643.firebaseapp.com",
-  databaseURL: "https://ninjagram-b6643-default-rtdb.firebaseio.com",
-  projectId: "ninjagram-b6643",
-  storageBucket: "ninjagram-b6643.appspot.com",
-  messagingSenderId: "870718643091",
-  appId: "1:870718643091:web:94a945850c13e82a972666"
+   apiKey: "AIzaSyAwe1RX7le5pd6g0ovAWCguVjuUrPl8x7s",
+    authDomain: "ninjagram-bbf39.firebaseapp.com",
+    projectId: "ninjagram-bbf39",
+    storageBucket: "ninjagram-bbf39.appspot.com",
+    messagingSenderId: "365760537804",
+    appId: "1:365760537804:web:934cfb3e661433a8ee8e69"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
