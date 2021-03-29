4db18e5012ea085e06eef69388c6b83d9c445c6e
Francisco Castillo <73005797+FranBlake89@users.noreply.github.com>
2021-03-29 12:15:57 -0300

    Update fireBase.js

--- www/js/fireBase.js
+++ www/js/fireBase.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-   apiKey: "AIzaSyAwe1RX7le5pd6g0ovAWCguVjuUrPl8x7s",
-    authDomain: "ninjagram-bbf39.firebaseapp.com",
-    projectId: "ninjagram-bbf39",
-    storageBucket: "ninjagram-bbf39.appspot.com",
-    messagingSenderId: "365760537804",
-    appId: "1:365760537804:web:934cfb3e661433a8ee8e69"
+   apiKey: "xxxx.xxxxx.xxxxxxxx.xxxxx",
+    authDomain: "xxxx.xxxxx.xxxxxxxx.xxxxx",
+    projectId: "xxxx.xxxxx.xxxxxxxx.xxxxx",
+    storageBucket: "xxxx.xxxxx.xxxxxxxx.xxxxx",
+    messagingSenderId: "xxxx.xxxxx.xxxxxxxx.xxxxx",
+    appId: "xxxx.xxxxx.xxxxxxxx.xxxxx"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
